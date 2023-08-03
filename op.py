c3364cc50b3a7503b845bce96945a265f68cbba8
Dylan Rogers <opendylan@proton.me>
2023-08-03 13:03:58 +0100

    Update package list

--- op.py
+++ op.py
@@ -7,3 +7,4 @@ if args[1] == "i":
 	app = requests.get("https://github.com/dylanopen/op/raw/main/core/" + args[2]).content.decode("utf-8")
 	with open("/usr/bin/" + args[2], "w") as f:
 		f.write(app)
+	print(f"Sucessfully installed package '{args[2]}' from 'core'.")
